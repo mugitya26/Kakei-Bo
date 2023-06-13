5d57d654a12efdb00532c233790d1e439d0fbd75
vanillaGCC <n.youshow68@gmail.com>
2023-06-13 13:43:59 +0900

    Update GetData.java

--- src/main/java/adv1b/group06/kakeibo/GetData.java
+++ src/main/java/adv1b/group06/kakeibo/GetData.java
@@ -1,13 +1,14 @@
 package adv1b.group06.kakeibo;
 
 import java.util.*;
+
 import adv1b.group06.kakeibo.model.Category;
 import adv1b.group06.kakeibo.model.Item;
 import adv1b.group06.kakeibo.model.StatisticsInfo;
 
 
 public class GetData {
-    public static List<StatisticsInfo> getKakeiboData(int year,int month,int date){
+    public static List<StatisticsInfo> getKakeiboData(int year,int month){
 
         return null;
     }
