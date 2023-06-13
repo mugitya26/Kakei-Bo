dc49193bd429bc0681f8f4ac519fb0ae8379f3de
vanillaGCC <n.youshow68@gmail.com>
2023-06-13 14:13:37 +0900

    Update MainController.java

--- src/main/java/adv1b/group06/kakeibo/controller/MainController.java
+++ src/main/java/adv1b/group06/kakeibo/controller/MainController.java
@@ -11,10 +11,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
@@ -31,8 +28,6 @@ public class MainController {
     public TableColumn<ItemEntity, Category> categoryColumn;
     public TableColumn<ItemEntity, Integer> priceColumn;
     public PieChart pieChart;
-
-    public Button cancel;
 
     public MenuBar menuBar;
 
