be5f0a66c66e6824a9536a8747329555801bb2e6
Toshihiro Ogino <tonigo0725@gmail.com>
2023-07-04 16:14:40 +0900

    fix:不要なtry-catchを削除

--- src/main/java/adv1b/group06/kakeibo/controller/IncomeController.java
+++ src/main/java/adv1b/group06/kakeibo/controller/IncomeController.java
@@ -18,6 +18,7 @@ import java.util.List;
 
 /**
  * IncomeWindowを扱うControllerクラス
+ *
  * @author 斉藤
  */
 public class IncomeController {
@@ -42,36 +43,32 @@ public class IncomeController {
 
     /**
      * 完了ボタンに紐づけられているイベントハンドラー
+     *
      * @param event
      */
     @FXML
-    private void onFinishButtonPressed(ActionEvent event){
-        try{
-            //textfieldとdatepickerから値を獲得
-            int price = Integer.parseInt(incomeValue.getText());
-            LocalDate selectedDate = datePicker.getValue();
+    private void onFinishButtonPressed(ActionEvent event) {
+        //textfieldとdatepickerから値を獲得
+        int price = Integer.parseInt(incomeValue.getText());
+        LocalDate selectedDate = datePicker.getValue();
 
-            //収入として生成したItem
-            Item Income = new Item("収入",new Category("収入",false),price);
+        //収入として生成したItem
+        Item Income = new Item("収入", new Category("収入", false), price);
 
-            //LocalDate型のselectedDateを以下の形式で文字列に変換
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            String dateString = selectedDate.format(formatter);
+        //LocalDate型のselectedDateを以下の形式で文字列に変換
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String dateString = selectedDate.format(formatter);
 
-            //dateStringをyear,month,dateにsplitする
-            String[] dateElement = dateString.split("-");
-            int pickedYear  = Integer.parseInt(dateElement[0]);
-            int pickedMonth = Integer.parseInt(dateElement[1]);
-            int pickedDate  = Integer.parseInt(dateElement[2]);
+        //dateStringをyear,month,dateにsplitする
+        String[] dateElement = dateString.split("-");
+        int pickedYear = Integer.parseInt(dateElement[0]);
+        int pickedMonth = Integer.parseInt(dateElement[1]);
+        int pickedDate = Integer.parseInt(dateElement[2]);
 
-            //上で獲得した日付からItemのListを取得し，それに今回のIncomeをaddしてからset
-            List<Item> list = DataManager.getItemDataList(pickedYear,pickedMonth,pickedDate);
-            list.add(Income);
-            DataManager.setSingleDayData(pickedYear,pickedMonth,pickedDate,list);
-
-        }catch (Exception e){
-
-        }
+        //上で獲得した日付からItemのListを取得し，それに今回のIncomeをaddしてからset
+        List<Item> list = DataManager.getItemDataList(pickedYear, pickedMonth, pickedDate);
+        list.add(Income);
+        DataManager.setSingleDayData(pickedYear, pickedMonth, pickedDate, list);
     }
 
 
