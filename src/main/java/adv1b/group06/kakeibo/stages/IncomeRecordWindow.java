e87e78c8481766a35a892f5bab605e380a876247
D4inosuke <al21046@shibaura-it.ac.jp>
2023-07-04 14:22:56 +0900

    Update IncomeRecordWindow.java
    IncomeRecordの更新とJavadoc

--- src/main/java/adv1b/group06/kakeibo/stages/IncomeRecordWindow.java
+++ src/main/java/adv1b/group06/kakeibo/stages/IncomeRecordWindow.java
@@ -6,8 +6,17 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+/**
+ * IncomeViewウインドウをステージさせるクラス
+ * @author 斉藤
+ */
 public class IncomeRecordWindow extends Stage {
 
+    /**
+     * IncomeViewをステージする関数
+     * @param stage
+     * @throws Exception
+     */
     public IncomeRecordWindow(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/adv1b/group06/kakeibo/views/IncomeView.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
