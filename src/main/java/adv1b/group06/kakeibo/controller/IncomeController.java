c6eb1615f42566db2d53c7cbed0178daea527835
D4inosuke <al21046@shibaura-it.ac.jp>
2023-07-04 13:59:31 +0900

    IncomeControllerの実装

---
+++ src/main/java/adv1b/group06/kakeibo/controller/IncomeController.java
@@ -0,0 +1,39 @@
+package adv1b.group06.kakeibo.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+import java.time.LocalDate;
+
+/**
+ * IncomeWindowを扱うControllerクラス
+ */
+public class IncomeController {
+
+    /***
+     * dateが格納されてるDatePicker
+     */
+    @FXML
+    private DatePicker datePicker;
+
+    /**
+     * 収入の値が文字列で格納されているTextField
+     */
+    @FXML
+    private TextField incomeValue;
+
+    /**
+     * 完了ボタンに紐づけられているイベントハンドラー
+     * @param event
+     */
+    @FXML
+    private void onFinishButtonPressed(ActionEvent event){
+        try{
+            int number = Integer.parseInt(incomeValue.getText());
+            LocalDate selectedDate = datePicker.getValue();
+        }catch (Exception e){
+
+        }
+    }
+}
--- src/main/java/adv1b/group06/kakeibo/stages/IncomeRecordWindow.java
+++ src/main/java/adv1b/group06/kakeibo/stages/IncomeRecordWindow.java
@@ -1,5 +1,6 @@
 package adv1b.group06.kakeibo.stages;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -8,7 +9,13 @@ import javafx.stage.Stage;
 public class IncomeRecordWindow extends Stage {
 
     public IncomeRecordWindow(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/adv1b/group06/kakeibo/views/IncomeView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        setTitle("収入記録");
+        setScene(scene);
         initOwner(stage);
+        stage=this;
     }
+
 }
 
