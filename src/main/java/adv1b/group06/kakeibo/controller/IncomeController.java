312535b08e756cfb858229c684b9e92bb687ae29
D4inosuke <al21046@shibaura-it.ac.jp>
2023-07-04 15:35:49 +0900

    IncomeController完成

--- src/main/java/adv1b/group06/kakeibo/controller/IncomeController.java
+++ src/main/java/adv1b/group06/kakeibo/controller/IncomeController.java
@@ -1,10 +1,16 @@
 package adv1b.group06.kakeibo.controller;
+import adv1b.group06.kakeibo.DataManager;
+import adv1b.group06.kakeibo.model.Category;
+import adv1b.group06.kakeibo.model.Item;
+import adv1b.group06.kakeibo.DataManager.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 import java.awt.event.ActionEvent;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 /**
  * IncomeWindowを扱うControllerクラス
@@ -31,8 +37,28 @@ public class IncomeController {
     @FXML
     private void onFinishButtonPressed(ActionEvent event){
         try{
-            int number = Integer.parseInt(incomeValue.getText());
+            //textfieldとdatepickerから値を獲得
+            int price = Integer.parseInt(incomeValue.getText());
             LocalDate selectedDate = datePicker.getValue();
+
+            //収入として生成したItem
+            Item Income = new Item("収入",new Category("収入",false),price);
+
+            //LocalDate型のselectedDateを以下の形式で文字列に変換
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String dateString = selectedDate.format(formatter);
+
+            //dateStringをyear,month,dateにsplitする
+            String[] dateElement = dateString.split("-");
+            int pickedYear  = Integer.parseInt(dateElement[0]);
+            int pickedMonth = Integer.parseInt(dateElement[1]);
+            int pickedDate  = Integer.parseInt(dateElement[2]);
+
+            //上で獲得した日付からItemのListを取得し，それに今回のIncomeをaddしてからset
+            List<Item> list = DataManager.getItemDataList(pickedYear,pickedMonth,pickedDate);
+            list.add(Income);
+            DataManager.setSingleDayData(pickedYear,pickedMonth,pickedDate,list);
+
         }catch (Exception e){
 
         }
