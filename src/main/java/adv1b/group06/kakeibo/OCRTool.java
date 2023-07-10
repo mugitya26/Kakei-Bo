5ac5a3f434ca35ef82e9bcf47c3e82bdcbdb9a55
Toshihiro Ogino <37017644+ToshihiroOgino@users.noreply.github.com>
2023-07-10 23:04:03 +0900

    Fix/ocrの例外処理 (#50)
    * fix: OCRに失敗した時、エラーメッセージを表示するコードを追加
    
    * fiat: README.mdでOCRに関する記述を追加

--- src/main/java/adv1b/group06/kakeibo/OCRTool.java
+++ src/main/java/adv1b/group06/kakeibo/OCRTool.java
@@ -1,7 +1,9 @@
 package adv1b.group06.kakeibo;
 
 import javafx.util.Pair;
-import net.sourceforge.tess4j.*;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -10,15 +12,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Timer;
 
 /**
  * レシートが中央に垂直に配置されている画像をデータに変換できる
+ *
  * @author 荻野
  */
 public class OCRTool {
     public static void main(String[] args) {
         OCRTool tool = new OCRTool();
-        var res = tool.getDataFromImgPath("images/img01.jpg");
+//        var res = tool.getDataFromImgPath("images/img01.jpg");
+        var res = tool.getDataFromImgPath("images/imgTest.jpg");
         for (var pair : res) {
             System.out.println(pair.toString());
         }
@@ -45,7 +50,10 @@ public class OCRTool {
         try {
             return getDataFromBufferedImage(ImageIO.read(new File(imagePath)));
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            DialogGenerator.createNewErrorAlert("指定された画像を読み込めませんでした。");
+            // 空のリストを返す。
+            return new ArrayList<>();
         }
     }
 
@@ -56,8 +64,15 @@ public class OCRTool {
      * @return 項目名と金額のPairの配列
      */
     public List<Pair<String, Integer>> getDataFromBufferedImage(BufferedImage image) {
-        var OCRResults = doOCR(image);
-        return getDataFromCafeteriaOrCoop(OCRResults);
+        try {
+            var OCRResults = doOCR(image);
+            return getDataFromCafeteriaOrCoop(OCRResults);
+        } catch (Exception e) {
+            e.printStackTrace();
+            DialogGenerator.createNewErrorAlert("画像を上手く読み込めませんでした。レシートが中央に大きく、真っ直ぐ写った画像を読み込んでください。");
+            // 空のリストを返す。
+            return new ArrayList<>();
+        }
     }
 
     /**
@@ -76,7 +91,10 @@ public class OCRTool {
         leftStr = leftStr.substring(leftStr.indexOf("\n") + 1);
 
         var nameList = new ArrayList<String>();
-        while (true) {
+        int cnt = 0, timeoutCnt = 1000;
+        while (cnt < timeoutCnt) {
+            cnt++;
+
             // 1行目を商品名と仮定して取得
             String itemName = leftStr.substring(0, leftStr.indexOf("\n"));
             leftStr = leftStr.substring(leftStr.indexOf("\n") + 1);
@@ -94,6 +112,10 @@ public class OCRTool {
             }
         }
 
+        if (cnt >= timeoutCnt) {
+            throw new RuntimeException("OCRがタイムアウトしました。");
+        }
+
         var valueList = getItemValues(rightStr, nameList.size());
 
         return combineArraysToPair(nameList, valueList);
@@ -109,6 +131,7 @@ public class OCRTool {
         int width = image.getWidth();
         BufferedImage leftImg = image.getSubimage(0, 0, width * 3 / 4, image.getHeight());
         BufferedImage rightImg = image.getSubimage(width * 3 / 4, 0, width / 4, image.getHeight());
+
 
         String leftStr = null, rightStr = null;
         String[] res = new String[2];
